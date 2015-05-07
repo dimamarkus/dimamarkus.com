7e6266735927733eaee189be901e279a9b4f7803
Dima Markus <dmi3000@gmail.com>
2015-05-07 04:02:20 -0400

    Initial commit

---
+++ craft/config/general.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+  '*' => array(
+		'appId'       => 'dimamarkus.com',
+		'cacheMethod' => 'redis',
+  ),
+  'local' => array(
+  	'devMode' => true,
+	),
+  'production' => array(
+
+  ),
+);
+
+
+
+
